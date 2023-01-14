c24d7620df592e1fa8adc907c84821a70c52a639
mnia <mohammad.sn51@gmail.com>
2023-01-14 16:41:14 -0500

    add some styling

--- src/features/pokemon/SearchHistory.tsx
+++ src/features/pokemon/SearchHistory.tsx
@@ -1,21 +1,23 @@
 import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import { RootState } from '../../app/store';
+import './SearchHistory.css';
 
 const SearchHistory: React.FC = () => {
   const searchHistory = useSelector((state: RootState) => state.search.searchHistory)
 
   return (
-    <>
+    <div className="history">
       <h5>Search History</h5>
       <ul>
         {searchHistory.map((term, index) => (
           <li key={index}>
             <Link to={`/pokemon/${term}`}>{term}</Link>
           </li>
-        ))}        
+        ))}
       </ul>
-    </>
+      {searchHistory.length === 0 && <p className='empty'>Nothing searched yet</p>}
+    </div>
   )
 }
 
